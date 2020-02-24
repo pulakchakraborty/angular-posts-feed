48f239008528d0e9994a4e70a13f6a824f29ada4
Pulak Chakraborty <pulak.chakraborty@motius.local>
2020-02-24 22:28:35 +0100

    first commit MEAN stack app with Angular Event binding, component creation, and string interpolation

---
+++ src/app/post/post-create/post-create.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+})
+export class PostCreateComponent {
+
+  newPost = 'No Content';
+
+  onAddPost() {
+    this.newPost = "The user\'s post";
+    alert("Post is added");
+  }
+}
