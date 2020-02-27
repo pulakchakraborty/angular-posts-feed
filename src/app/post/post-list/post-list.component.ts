f66d11618a28868af633c22c7a15055c330f2182
Pulak Chakraborty <pulak.chakraborty@motius.local>
2020-02-27 10:29:12 +0100

    create structural directive using ngFor

---
+++ src/app/post/post-list/post-list.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component ({
+  selector: 'app-post-list',
+  templateUrl: './post-list.component.html',
+  styleUrls: ['./post-list.component.css']
+})
+export class PostListComponent {
+  posts = [
+    {title: "first post", content: "content of the first post"},
+    {title: "second post", content: "content of the second post"},
+    {title: "third post", content: "content of the third post"}
+  ];
+}
