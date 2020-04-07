bd7e69eec0c667629b367fdb85c46d78665ba49d
Pulak Chakraborty <pulak.chakraborty@motius.local>
2020-04-07 15:02:57 +0200

    create an api route to fetch posts - new api - /api/posts - return response to the client in json format

--- backend/app.js
+++ backend/app.js
@@ -2,13 +2,28 @@ const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log('first Middleware');
-  next();
-});
-
-app.use((req, res, next) => {
-  res.send('Hello from Express');
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "ABC1000001",
+      title: "First Post from server",
+      content: "The content of the first post"
+    },
+    {
+      id: "ABC1000002",
+      title: "Second Post from server",
+      content: "The content of the second post"
+    },
+    {
+      id: "ABC1000003",
+      title: "Third Post from server",
+      content: "The content of the third post"
+    }
+  ];
+  res.status(200).json({
+    message: 'the posts from the server are fetched successfully!!',
+    posts: posts
+  });   // return to the client
 });
 
 module.exports = app;
