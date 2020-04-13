5ba0125e8a668aa751554d99dafaf437f22555ad
Pulak Chakraborty <pulak.chakraborty@motius.local>
2020-04-13 15:02:01 +0200

    Add Async Mime Type Validator - create a new mime-type.validator.ts file to host async validation using Uint8Array - use the validator as an async function in form control of image element - fix the css file for image preview bug

--- src/app/post/post-create/post-create.component.ts
+++ src/app/post/post-create/post-create.component.ts
@@ -4,6 +4,7 @@ import { Post } from '../post.model';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
 import { PostsService } from '../posts.service';
 import { ActivatedRoute, ParamMap } from '@angular/router';
+import { mimeType } from './mime-type.validator';
 
 @Component ({
   selector: 'app-post-create',
@@ -33,7 +34,7 @@ export class PostCreateComponent implements OnInit {
         validators: [Validators.required]
       }),
       image: new FormControl(null, {
-        validators: [Validators.required]
+        validators: [Validators.required], asyncValidators: [mimeType]
       })
     });
     this.route.paramMap.subscribe((paramMap: ParamMap) => {
