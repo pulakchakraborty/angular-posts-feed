3dfb34dcb6e29a1a4569fe909c8fd4f5054d650d
Pulak Chakraborty <pulakchakraborty.contact@gmail.com>
2020-05-03 16:31:43 +0200

    create /login endpoint in backend - add jwt package to return webtoken on successful login

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 
 const User = require("../models/user");
 
@@ -23,6 +24,28 @@ router.post("/signup", (req, res, next) => {
           res.status(500).json({ error });
         });
     });
+});
+
+router.post("/login", (req, res, next) => {
+  // Authentication logic
+  let fetchedUser;
+  User.findOne({ email: req.body.email }).then(user => {
+    if (!user) {
+      return res.status(401).json({ message: "Authentication failed" });
+    }
+    fetchedUser = user;
+    return bcrypt.compare(req.body.password, user.password);
+  })
+  .then(result => {
+    if (!result) {
+      return res.status(401).json({ message: "Authentication failed" });
+    }
+    const token = jwt.sign({ email: fetchedUser.email, userId: fetchedUser._id }, "secret_key_changeInProd", { expiresIn: "1h" });
+    res.status(200).json({ token });
+  })
+  .catch(error => {
+    return res.status(401).json({ error, message: "Authentication failed" });
+  })
 });
 
 module.exports = router;
